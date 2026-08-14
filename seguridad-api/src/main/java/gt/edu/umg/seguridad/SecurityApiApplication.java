286fa6fb1a9ebbdc18e4bb02311815aa48e35af4
jsanchezd13 <163688661+jsanchezd13@users.noreply.github.com>
2026-08-14 16:38:40 -0600

    Update SecurityApiApplication.java

--- seguridad-api/src/main/java/gt/edu/umg/seguridad/SecurityApiApplication.java
+++ seguridad-api/src/main/java/gt/edu/umg/seguridad/SecurityApiApplication.java
@@ -10,8 +10,8 @@ public class SecurityApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApiApplication.class, args);
-        System.out.println("✅ Seguridad-API iniciada correctamente!");
-        System.out.println("🔒 Swagger: http://localhost:8081/swagger-ui/index.html");
-        System.out.println("🔑 Endpoint de login: POST http://localhost:8081/auth/login");
+        System.out.println("Seguridad-API iniciada correctamente!");
+        System.out.println("Swagger: http://localhost:8081/swagger-ui/index.html");
+        System.out.println("Endpoint de login: POST http://localhost:8081/auth/login");
     }
 }
